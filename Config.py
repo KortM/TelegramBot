53595d3ef15857fd582a519837e3e642d5b005a2
KortM <cort202@gmail.com>
2019-02-25 03:30:34 +0300

    make it better

---
+++ Config.py
@@ -0,0 +1,33 @@
+import os
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+basedir =os.path.abspath(__name__)
+name = '/../BD.db'
+engine = create_engine("sqlite:///"+basedir+name)
+print(basedir+name)
+Base = declarative_base()
+Session =sessionmaker(bind= engine)
+class Mac(Base):
+    __tablename__ = 'Mac'
+    id = Column(Integer, primary_key=True)
+    A = Column(String(10))
+    B = Column(String(10))
+    C = Column(String(10))
+    #D = Column(String(10))
+    #E = Column(String(10))
+    #F = Column(String(10))
+    title = Column(String(30))
+    address = Column(String(30))
+
+    def __init__(self,A,B,C, title, address):
+        self.A = A
+        self.B = B
+        self.C = C
+        self.title = title
+        self.address = address
+    def __repr__(self):
+        return("{0}:{1}:{2}".format(self.A, self.B, self.C))
+
+Base.metadata.create_all(engine)
---
+++ Main.py
@@ -0,0 +1,86 @@
+from Config import Session, Mac
+import csv
+class UpdateBD():
+    def __init__(self):
+        self.s = Session()
+
+    def update(self, A,B,C,title, address):
+        #print(A+ B+C+D+E+F+title)
+        m = Mac(A,B,C, title, address)
+        self.s.add(m)
+        self.s.commit()
+        print('Add')
+    def splitStr(self, mac):
+        mac = mac.upper()
+        str = mac.split(':')
+        if(len(str)<6):
+            print("Ветка А")
+            str = mac.split('.')
+            if(len(str) <3):
+                print("Ветка Б")
+                str = mac.split('-')
+                if(len(str)<6):
+                    print("Ветка С")
+                    return 'Неверный формат mac-адреса!'
+                else:
+                    result=self.search(str[0], str[1], str[2], str[3], str[4], str[5])
+            else:
+                result=self.search(str[0][0:2], str[0][2:4], str[1][0:2], str[1][2:4], str[2][0:2], str[2][2:4])
+        else:
+            result = self.search(str[0], str[1], str[2], str[3], str[4], str[5])
+        return result
+
+    def search(self, A,B,C,D,E,F):
+        a = self.s.query(Mac).filter_by(A=str(A)).all()
+        count = 0
+        max_count = 0
+        list_count = 0
+        result_index = []
+        for i in a:
+            if str(B) in str(i)[3:5]:
+                count = count +1
+                if C in str(i)[6:8]:
+                    count = count +1
+                    if D in str(i)[9:11]:
+                        count = count + 1
+                        if E in str(i)[12:14]:
+                            count = count + 1
+                            if F in str(i)[15:17]:
+                                result_index.insert(0, i)
+                                result_index.insert(1, i.title)
+                                result_index.insert(2, i.address)
+            if count >0:
+                if max_count < count:
+                    max_count = count
+                    result_index.insert(0, i)
+                    result_index.insert(1, i.title)
+                    result_index.insert(2, i.address)
+            count = 0
+            list_count = list_count +1
+        if len(result_index) <=0:
+            return "Mac не найден!"
+        else:
+            #return str(result_index[0]).lstrip()+' Название организации: '+str(result_index[1]+' Адрес: '+str(result_index[2]))
+            return 'Название организации: ' + str(
+                result_index[1] +'\n'+ 'Адрес: ' + str(result_index[2]))
+
+    def load_in_csv(self):
+        f = open("macbd.csv")
+        reader  = csv.DictReader(f)
+        for line in reader:
+            A = str(line['Assignment'])[0:2]
+            B = str(line['Assignment'])[2:4]
+            C = str(line['Assignment'])[4:6]
+            Name = str(line['Organization Name'])
+            Address  = str(line['Organization Address'])
+            self.update(A, B, C, Name, Address)
+
+
+
+"""if __name__=='__main__':
+    u = UpdateBD()
+    #u.load_in_csv()
+    #u.load_data()
+    #print(u.splitStr('ec08.6b17.3e2f'))
+    #print(u.splitStr('00.1A.4B.00.00.00'))
+    #print(u.splitStr('D4:9E:6D:D0:02:01'))"""
